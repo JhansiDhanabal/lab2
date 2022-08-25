33381adb21c3ad35b1fdbd408760665c18ab1140
JhansiDhanabal <111398658+JhansiDhanabal@users.noreply.github.com>
2022-08-25 06:54:28 -0700

    Add files via upload

--- problem1.java
+++ problem1.java
@@ -1,15 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package oops2;
-
+package javaapplication3;
 import java.util.Scanner;
-
 /**
  *
- * @author 21CSE073
+ * @author USER
  */
 public class problem1 {
 
@@ -18,64 +15,101 @@ public class problem1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner o=new Scanner(System.in);
+        System.out.println("enter author details:");
+        author obj1=new author();
+        obj1.name=o.next();//"Mark Allen Weiss";
+        obj1.email=o.next();//"allenmark.2@gmail.com";
+        obj1.gender=o.next().charAt(0);//'M';
+        System.out.println();
+        System.out.println("enter book details:");
+        String nam=o.next();
+        double pr=o.nextDouble();
+        int qua=o.nextInt();
+        Book obj=new Book(nam,obj1,pr);
+        System.out.println(obj.getName());
+        System.out.println(obj.getAuthor());
+        System.out.println(obj.getprice());
+        System.out.println();
+        System.out.println(obj);
+        System.out.println();
+        Book obj2=new Book(nam,obj1,pr,qua);
+        System.out.println(obj2.getName());
+        System.out.println(obj2.getAuthor());
+        System.out.println("price:"+obj2.getprice());
+        System.out.println("quantity"+obj2.getqty());
+        System.out.println();
+        System.out.println(obj2);
+        System.out.println();
+        System.out.println("enter the price to be changed:");
+        double pc=o.nextDouble();
+        obj2.setprice(pc);
+        System.out.println("enter the quantity to be changed:");
+        int quan=o.nextInt();
+        obj2.setqty(quan);
+        System.out.println();
+        System.out.println(obj2);
+        System.out.println(obj);
         
-        System.out.println("enter number of student:");
-        Scanner obj1=new Scanner(System.in);
-        int n=obj1.nextInt();
-        student s[]=new student[n];
-        for(int i=0;i<n;i++)
-        {
-            s[i]=new student();
-        }
-        System.out.println("enter student details:");
-        for(int i=0;i<n;i++)
-        {
-            s[i].getdata();
-            s[i].percentage();
-            System.out.println();
-            s[i].display();
-            System.out.println();
-        }
+        
     }
     
 }
-class student
+class author
 {
-    String student_name;
-    int register_number;
-    int working_days;
-    int days_present;
-    int marks;
-    double per;
-    void getdata()
-    {
-        Scanner obj=new Scanner(System.in);
-        System.out.println("enter student name:");
-        student_name=obj.next();
-        System.out.println("enter register number:");
-        register_number=obj.nextInt();
-        System.out.println("enter wording days:");
-        working_days=obj.nextInt();
-        System.out.println("enter number of days present:");
-        days_present=obj.nextInt();
-    }
-    void percentage()
-    {
-        per=(double)((days_present*100)/working_days);
-    }
-    void display()
-    {
-        
-        if(per>=90)
-            marks=5;
-        else if(per>=80)
-            marks=4;
-        else if(per>=75)
-            marks=3;
-        else
-            marks=0;
-        
-        System.out.println("Attendance percentage >= "+per+" - "+marks+" Marks");
-    }
+    String name;
+    String email;
+    char gender;
 }
-
+class Book
+{
+    String b_name;
+    author a=new author();
+    static double price;
+    static int qty;
+    Book(String n,author an,double p)
+    {
+        b_name=n;
+        a.name=an.name;
+        a.email=an.email;
+        a.gender=an.gender;
+        price=p;
+    }
+    Book(String n,author an,double p,int q)
+    {
+        b_name=n;
+        a.name=an.name;
+        a.email=an.email;
+        a.gender=an.gender;
+        price=p;
+        qty=q;
+    }
+    String getName()
+    {
+        return "BOOK NAME : "+b_name;
+    }
+    String getAuthor()
+    {
+        return "AUTHOR : "+a.name+"EMAIL: "+a.email+"GENDER"+a.gender;
+    }
+    Double getprice()
+    {
+        return price;
+    }
+    void setprice(double value)
+    {
+        price=value;
+    }
+    int getqty()
+    {
+        return qty;
+    }
+    void setqty(int qy)
+    {
+        qty=qy;
+    }
+   
+    public String toString()
+    {
+        return "BOOK [name = "+b_name+", AUTHOR {NAME="+a.name+". email="+a.email+", gender="+a.gender+"],price="+price+",qty="+qty+"]";   }
+}
